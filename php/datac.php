2f67b3424d84e20c9b3700a1106af10cbe1255b7
Adnan Hussain <itzadnantech@gmail.com>
2022-08-02 10:22:43 +0500

    updates 8/2/2022

--- php/datac.php
+++ php/datac.php
@@ -1,7 +1,7 @@
 
 <?php 
 
-$conn = new mysqli("localhost", "usman", "Maani@711812", "valet_db");
+$conn = new mysqli("localhost", "root", "", "webxolutions_valet_db");
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
